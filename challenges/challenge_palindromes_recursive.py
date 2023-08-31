a1f9890af3ae047fd6531cc373c7c5d1a1346461
hildelio <hildelio@gmail.com>
2023-08-31 20:55:15 -0300

    Projeto Algoritmos 👨‍💻 #️⃣ 🐍 Req 03

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,19 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if len(word) == 0:
+        return False
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
+
+
+def is_palindrome(word):
+    if not word:
+        return False
+
+    return is_palindrome_recursive(word, 0, len(word) - 1)
     raise NotImplementedError
