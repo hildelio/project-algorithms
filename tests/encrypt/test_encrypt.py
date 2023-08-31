71565037e2fe2a646eddcf245985542fe60d44cd
hildelio <hildelio@gmail.com>
2023-08-31 20:25:33 -0300

    Projeto Algoritmos 👨‍💻 #️⃣ 🐍 Req 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("O miserável é um gênio", "kakaroto")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 23)
+
+    assert encrypt_message("Morra inseto!", 5).strip() == "arroM_!otesni"
+    assert encrypt_message(
+        "Kakaroto, seu verme insolente!", 4
+        ).strip() == "!etnelosni emrev ues ,otor_akaK"
+    assert encrypt_message(
+        "O orgulho é um deus dos tolos.", 40
+        ).strip() == ".solot sod sued mu é ohlugro O"
