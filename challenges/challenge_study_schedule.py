7178a1206e42439c2562348baa749215bc77f519
hildelio <hildelio@gmail.com>
2023-08-31 15:42:17 -0300

    Projeto Algoritmos 👨‍💻 #️⃣ 🐍 Req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if target_time is None:
+        return None
+
+    if any(not isinstance(start, int) or not isinstance(end, int) or start > end for start, end in permanence_period):
+        return None
+
+    count = 0
+    for start, end in permanence_period:
+        if start <= target_time <= end:
+            count += 1
+
+    return count
     raise NotImplementedError
