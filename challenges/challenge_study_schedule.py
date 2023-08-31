28dab02dd65c638baf9c7fb5549267e6455be7b8
hildelio <hildelio@gmail.com>
2023-08-31 15:43:28 -0300

    Projeto Algoritmos 👨‍💻 #️⃣ 🐍 Req 01 :detective: fix lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,8 @@ def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
 
-    if any(not isinstance(start, int) or not isinstance(end, int) or start > end for start, end in permanence_period):
+    if any(not isinstance(start, int) or not isinstance(end, int)
+           or start > end for start, end in permanence_period):
         return None
 
     count = 0
