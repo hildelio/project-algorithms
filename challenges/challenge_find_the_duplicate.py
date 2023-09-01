d1344aebec6f79bdcda33efe02a914042932e7e4
hildelio <hildelio@gmail.com>
2023-09-01 14:30:51 -0300

    Projeto Algoritmos 👨‍💻 #️⃣ 🐍 Req 05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(nums, list) or len(nums) < 2:
+        return False
+
+    if any(not isinstance(num, int) or num < 1for num in nums):
+        return False
+
+    nums.sort()
+
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+
+    return False
