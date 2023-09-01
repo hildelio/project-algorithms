dbee74a42b0ae7d68b14c56e924cd567bfbf2f51
hildelio <hildelio@gmail.com>
2023-09-01 14:39:02 -0300

    Projeto Algoritmos 👨‍💻 #️⃣ 🐍 Req 06

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,17 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    if not word:
+        return False
+
+    word = ''.join(filter(str.isalnum, word)).lower()
+
+    left = 0
+    right = len(word) - 1
+
+    while left < right:
+        if word[left] != word[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
     raise NotImplementedError
